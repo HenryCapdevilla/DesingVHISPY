3a31f445f8e82b7932ff14c1edbe820ec5370839
HenryCapdevilla <henrydaniel2002@hotmail.com>
2023-09-16 15:06:58 -0500

    Agregue Hot Reload
    Signed-off-by: HenryCapdevilla <henrydaniel2002@hotmail.com>

--- obtener_datos.php
+++ obtener_datos.php
@@ -4,30 +4,40 @@ $username = "hdcm";
 $password = "hdcm02ds";
 $dbname = "dbHenry";
 
+header("Content-Type: text/event-stream");
+header("Cache-Control: no-cache");
+header("Connection: keep-alive");
+
+function sendSSE($data) {
+    echo "data: " . json_encode($data) . "\n\n";
+    ob_flush();
+    flush();
+}
+
 $conn = new mysqli($servername, $username, $password, $dbname);
 
 if ($conn->connect_error) {
     die("Conexión fallida: " . $conn->connect_error);
 }
 
-$sql = "SELECT LONGITUD, LATITUD, FECHA, HORA FROM coordenadas ORDER BY FECHA DESC, HORA DESC";
-$result = $conn->query($sql);
+while (true) {
+    $sql = "SELECT LONGITUD, LATITUD, FECHA, HORA FROM coordenadas ORDER BY FECHA DESC, HORA DESC LIMIT 1";
+    $result = $conn->query($sql);
 
-$data = array();
+    if ($result->num_rows > 0) {
+        $row = $result->fetch_assoc();
+        $data = array(
+            "longitud" => $row["LONGITUD"],
+            "latitud" => $row["LATITUD"],
+            "fecha" => $row["FECHA"],
+            "hora" => $row["HORA"]
+        );
 
-while ($row = $result->fetch_assoc()) {
-    $data[] = array(
-        "longitud" => $row["LONGITUD"],
-        "latitud" => $row["LATITUD"],
-        "fecha" => $row["FECHA"],
-        "hora" => $row["HORA"]
-    );
+        sendSSE($data);
+    }
+
+    sleep(1); // Espera 1 segundo antes de buscar otra actualización
 }
 
-$data = array_slice($data, 0, 10); // Get the last 10 values
-
 $conn->close();
-
-header("Content-Type: application/json");
-echo json_encode($data);
 ?>
