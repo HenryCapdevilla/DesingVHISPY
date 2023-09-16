f617e1d5b8d03a43ea2b48785443011f57d2109f
HenryCapdevilla <henrydaniel2002@hotmail.com>
2023-09-16 15:55:47 -0500

    Regreso a la última versión funcional 100% real

--- obtener_datos.php
+++ obtener_datos.php
@@ -1,8 +1,8 @@
 <?php
-$servername = "henrydb.cfsjsehoiurs.us-east-2.rds.amazonaws.com";
-$username = "hdcm";
-$password = "hdcm02ds";
-$dbname = "dbHenry";
+$servername = "database-1.cvdwcehcq5em.us-east-2.rds.amazonaws.com";
+$username = "Buabs";
+$password = "buabs123";
+$dbname = "BuabsBD";
 
 header("Content-Type: text/event-stream");
 header("Cache-Control: no-cache");
@@ -20,9 +20,14 @@ if ($conn->connect_error) {
     die("Conexión fallida: " . $conn->connect_error);
 }
 
+<<<<<<< HEAD
 while (true) {
     $sql = "SELECT LONGITUD, LATITUD, FECHA, HORA FROM coordenadas ORDER BY FECHA DESC, HORA DESC LIMIT 1";
     $result = $conn->query($sql);
+=======
+$sql = "SELECT LONGITUD, LATITUD, FECHA, HORA FROM tabla ORDER BY FECHA DESC, HORA DESC";
+$result = $conn->query($sql);
+>>>>>>> parent of f236c2c (Actualización de credenciales)
 
     if ($result->num_rows > 0) {
         $row = $result->fetch_assoc();
