ad9375ca65969de7067966d5245de3698122a521
HenryCapdevilla <henrydaniel2002@hotmail.com>
2023-09-28 01:52:15 -0500

    Añadi el timepicker y la lógica

--- obtener_datos_history.php
+++ obtener_datos_history.php
@@ -12,6 +12,8 @@ $database_name = $config['database']['name'];
 // Obtener las fechas de inicio y fin desde la solicitud POST (puedes usar GET si lo prefieres)
 $fechaInicial = $_POST["fecha_inicial"];
 $fechaFinal = $_POST["fecha_final"];
+$horaInicio = $_POST["hora_inicio"];
+$horaFin = $_POST["hora_fin"];
 
 $conn = new mysqli($database_host, $database_user, $database_password, $database_name);
 
@@ -19,9 +21,20 @@ if ($conn->connect_error) {
     die("Conexión fallida: " . $conn->connect_error);
 }
 
-// Construir la consulta SQL con las fechas de inicio y fin
-$sql = str_replace('$fechaInicial', $fechaInicial, str_replace('$fechaFinal', $fechaFinal, $config['sql_h']));
-$result = $conn->query($sql);
+// Construir la consulta SQL con las fechas y horas de inicio y fin
+$sql = "SELECT LONGITUD, LATITUD, FECHA, HORA FROM coordenadas 
+        WHERE FECHA BETWEEN ? AND ? 
+        AND HORA BETWEEN ? AND ?
+        ORDER BY FECHA DESC, HORA DESC";
+// Preparar la declaración SQL
+$stmt = $conn->prepare($sql);
+// Asociar los valores de las fechas y horas a los marcadores de posición en la consulta SQL
+$stmt->bind_param("ssss", $fechaInicial, $fechaFinal, $horaInicio, $horaFin);
+// Ejecutar la consulta SQL
+$stmt->execute();
+// Obtener el resultado de la consulta
+$result = $stmt->get_result();
+
 
 $data = array();
 
@@ -34,8 +47,11 @@ while ($row = $result->fetch_assoc()) {
     );
 }
 
+// Cerrar la conexión y liberar los recursos
+$stmt->close();
 $conn->close();
 
+// Devolver los datos en formato JSON
 header("Content-Type: application/json");
 echo json_encode($data);
 ?>
