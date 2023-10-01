32f9e0eff2e92b55640eee58e13edd76e081af25
HenryCapdevilla <henrydaniel2002@hotmail.com>
2023-09-30 20:42:29 -0500

    cambio en el app.get linea 42

--- src/app.js
+++ src/app.js
@@ -39,25 +39,35 @@ udp.on('message', (msg) =>{
 udp.bind(udpPort,udpHost);
 
 
-app.get("/data", (req,res) =>{
-    if(data[0]===0){
-        cnx.pool.query("INSERT INTO gps_data (LATITUD, LONGITUD, FECHA, HORA) VALUES (%s, %s, %s, %s)", (err,rows) => {
-            res.json({
-                "lon"   : rows[0].longitud,
-                "lat"   : rows[0].latitud,
-                "dt"    : moment(rows[0].fecha).format("YYYY/MM/DD"),
-                "tm"    : rows[0].hora,
-            });
+app.get("/data", (req, res) => {
+    if (data[0] === 0) {
+        cnx.pool.query("SELECT LONGITUD, LATITUD, FECHA, HORA FROM gps_data ORDER BY FECHA DESC, HORA DESC", (err, rows) => {
+            if (err) {
+                console.error(err);
+                return res.status(500).json({ error: "Error en la consulta a la base de datos" });
+            }
+
+            if (rows && rows.length > 0) {
+                res.json({
+                    "lon": rows[0].longitud,
+                    "lat": rows[0].latitud,
+                    "dt": moment(rows[0].fecha).format("YYYY/MM/DD"),
+                    "tm": rows[0].hora,
+                });
+            } else {
+                res.status(404).json({ error: "No se encontraron resultados en la base de datos" });
+            }
         });
     } else {
         res.json({
-            "lat"   : data[1],
-            "lon"   : data[0],
-            "tm"    : data[3],
-            "dt"    : moment(data[2]).format("YYYY/MM/DD"),
-        })
+            "lat": data[1],
+            "lon": data[0],
+            "tm": data[3],
+            "dt": moment(data[2]).format("YYYY/MM/DD"),
+        });
     }
 });
+
 
 app.use(express.json({limit: '1mb'}));
 app.post("/moment", (req,res) =>{
